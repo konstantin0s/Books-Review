840ab0c41b931345bda572c6a4f3faae58aac966
konstantin0s <constantintofan85@gmail.com>
2019-05-16 16:24:48 +0200

    Install jwt token for wordpress

--- .history/wp-config_20190516155949.php
+++ .history/wp-config_20190516155949.php
@@ -37,10 +37,6 @@ define( 'DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '');
 
-define('JWT_AUTH_SECRET_KEY', 'secret');
-
-define('JWT_AUTH_CORS_ENABLE', true);
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
